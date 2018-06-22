64987f4d0305ac1fde0a9dd577c1f00c173aa4cb
g3minis26 <g3minis26@gmail.com>
2018-06-22 15:15:02 -0500

    Add

---
+++ TemplateApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TemplateApp.Startup))]
+namespace TemplateApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
